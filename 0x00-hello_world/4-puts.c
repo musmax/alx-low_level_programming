0f0a213acb81c55ee7ea7dfedf060477e1abcb6f
musmax <musaojo1710@yahoo.com>
2022-03-11 00:04:26 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle"Return: 0 if existed properly, non-zero otherwise
+ */
+int main (void){
+	
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
+}
