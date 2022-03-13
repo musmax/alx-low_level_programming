7596bc2d0be866f0aee0716efbe258d914a08658
musmax <musaojo1710@yahoo.com>
2022-03-13 16:27:09 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n % 10 > 5)
 {
-printf("Last digit of %: is %: and is greater than 5\n", n, n % 10);
+printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 else if (n % 10 == 0)
 {
-printf("Last digut of %: is %: and is zero\n", n, n % 10);
+printf("Last digut of %i is %i and is zero\n", n, n % 10);
 }
 else 
 {
-printf("Last digit of %: is %: and is less than 6 and not 0\n", n, n % 10);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 return (0);
 }
