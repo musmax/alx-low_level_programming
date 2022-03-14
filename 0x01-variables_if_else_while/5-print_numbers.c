02d033662dfefc4973796ca1f8790f9ce0e2b5dd
musmax <musaojo1710@yahoo.com>
2022-03-13 18:56:37 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry 
+ * this program prints out single igits of bas ten
+ * Return: 0(success/correct)
+ */
+int main(void){
+for (int i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
