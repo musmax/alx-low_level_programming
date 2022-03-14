d254c6ab199207be7d7f7cf9375c85b3d6dd53cd
musmax <musaojo1710@yahoo.com>
2022-03-14 05:07:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * this program reverses 0-9 forward and backward
+ * Return: Always 0(success/correct)
+ */
+int main(void)
+{
+int go;
+int come;
+for (go = 0; go < 10; go++)
+{
+putchar (go);
+}
+putchar ('\n');
+for (come = 10; come > 0; come--)
+{
+putchar (come);	
+}
+putchar ('\n');
+return (0);
+}
