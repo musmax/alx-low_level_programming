5c7ca8aab12bde4771c13f598c4893859471fb2e
musmax <musaojo1710@yahoo.com>
2022-03-14 01:55:57 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,10 @@
  * Return: Always 0(success/correct)
  */
 int main(void)
-{int alpha;
-for (alpha = 0; alpha < 10; alpha++)
+{int i;
+for (i = 0; i < 10; i++)
 {
-printf("%alpha", alpha);
+printf("%i", i);
 }
 printf("\n");
 return (0);
