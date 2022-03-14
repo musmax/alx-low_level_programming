62a4a902b5da9500e7149fbc2e54358c9a6495cc
musmax <musaojo1710@yahoo.com>
2022-03-14 02:07:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib>
+/**
+ * main - Entry 
+ * this program use char to print 0-9
+ *Return: Always 0(success/correct)
+ */
+int main(void)
+{int i;
+for(i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
