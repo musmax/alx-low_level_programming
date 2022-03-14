3c9ac30f98a882237a6fe1049c090b1ea7d73efc
musmax <musaojo1710@yahoo.com>
2022-03-14 03:10:40 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * this program prints all number in base 16
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int i;
+if (i % 16 == 0)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
