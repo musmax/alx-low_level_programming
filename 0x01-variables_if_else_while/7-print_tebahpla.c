d1e13cc01bdc87a50a054d2d05d690d1153ca6b7
musmax <musaojo1710@yahoo.com>
2022-03-14 02:37:46 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * this program print alphabet in reverse order
+ * Return: Always 0(success/correct)
+ */
+int main(void)
+{
+char rev;
+for(rev = 'z'; rev < 'a'; rev++)
+{
+putchar (rev);
+}
+putchar ('\n');
+return (0);
+}
