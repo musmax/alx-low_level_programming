9b624eabb1b0ac2d226afc9e3ead3f6dc711c01c
musmax <musaojo1710@yahoo.com>
2022-03-13 18:39:21 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /** main - Entry point
- *
+ * this program prints out lower and uppercae alphabets
  * Return: Always 0(success/correct)
  */
 int main(void)
 {
 char lower;
 char upper;
-for (lower = 'a'; lower <= 'z'; lower++){
+for (lower = 'a'; lower <= 'z'; lower++)
+{
 putchar(lower);
 } 
-for (upper = 'A'; upper <= 'Z'; upper++){
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
 putchar(upper);
 }
 putchar('\n');
