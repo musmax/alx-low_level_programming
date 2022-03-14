d0f927cf98bd7c07535063c478d3533f735fd82b
musmax <musaojo1710@yahoo.com>
2022-03-14 01:32:12 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry 
+ * main - Entry
  * this program prints all letter without q and e
  * Return: always 0(success/correct)
  */
 int main(void)
 {
 char alpha;
-for(alpha = 'a'; alpha <= 'z'; alpha++)
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
-if(alpha == 101 || alpha == 113)
+if (alpha == 101 || alpha == 113)
 {
 continue;
 }
