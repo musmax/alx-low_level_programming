cb9028583351c9ef57767b4da7a219b61b31d5a7
musmax <musaojo1710@yahoo.com>
2022-03-17 02:35:18 -0700

    0-ma

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry 
- * this program is meant to debug
- * Return: 0(correct/sucess)
- */
+ * * main - tests function that prints if integer is positive or negative
+ * * Return: 0
+ * */
+
 int main(void)
 {
 int i;
 
 i = 0;
-positive_negative(i);
+positive_or_negative(i);
 
 return (0);
 }
