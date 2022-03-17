dd09ab167ea21368ad7d32f6e4857cbd8e09915c
musmax <musaojo1710@yahoo.com>
2022-03-17 01:45:26 -0700

    main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <0-main.c>
+/**
+ * main -Entry 
+ * this program is about debugging
+ * Return: 0(success/correct)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_negative(i);
+return (0);
+}
