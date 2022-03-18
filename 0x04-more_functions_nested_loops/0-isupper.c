c10f16fc012cf6c8122b4b5908bdcf63771ce725
musmax <musaojo1710@yahoo.com>
2022-03-17 18:42:22 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/** main entry
+ * this program checks for character
+ * Return 0: (success/correct)
+ *
+ */
+int main()
+{
+int _isupper(int c);
+if(c is uppercase)
+{
+return 1;
+}
+else
+{
+return 0;
+}
+}
