1374e3fc2e064c82945b3350e01bfb00bb3bb394
musmax <musaojo1710@yahoo.com>
2022-03-18 01:19:19 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include "header.h"
 
-/** main entry
- * this program checks for character
+/**
+ * main entry
+ * this program return 1 if c is upper and return 0 if not.
  * Return 0: (success/correct)
- *
  */
 int int_isupper(int c)
 {
 c = "C";
-if(c > 65 && c <= 95)
+if (c > 65 && c <= 95)
 {
-printf("%d c is upper",c);
-return 1;
+printf("%d c is upper", c);
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
