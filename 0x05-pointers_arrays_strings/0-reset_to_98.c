e82d0ce2dba78183b6c4242b1c3567f425cceaf9
musmax <musaojo1710@yahoo.com>
2022-03-22 04:20:45 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes a pointer and updates the value it point to
- * @n: pointer to an integer
- * Return: void
- */
-
+* reset_to_98 - updates the value it points
+*@n: pointer of the direcction at the variable n
+*Return: 0 (success)
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
