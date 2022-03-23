d7cbe6476d302caf9cebd610c9d3282b9a62570f
musmax <musaojo1710@yahoo.com>
2022-03-23 12:15:08 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main -entry 
+ *  a function that concatenates two strings.
+ *  return: 0 (success/correct)
+ */
+char *_strcat(char *dest, char *src)
+{
+char *concat = *dest + *src;
+return (*concat);
+}
