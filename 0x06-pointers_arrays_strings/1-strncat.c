e7639b0afec54476780e0e725aa2379b2af15032
musmax <musaojo1710@yahoo.com>
2022-03-24 02:59:44 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main enrty
+ *  a function that concatenates two strings.
+ *  return: 0 (success/correct)
+ */
+char *_strncat(char *dest, char *src, int n)
+{int count;
+n = 0;
+while (*(dest + n) != '\0'){
+n++;
+while (*(dest + n) = *(src + count))
+{
+	count++;
+	n++;
+	if (count == '\0')
+	{
+		break;
+	}
+}
+}
