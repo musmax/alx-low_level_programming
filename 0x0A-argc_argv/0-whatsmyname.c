8c0608fe3f67a385a057cfb4e96ce29ef4cddb43
musmax <musaojo1710@yahoo.com>
2022-04-02 18:29:20 -0700

    all

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(argc, char *argv)
+{
+	printf("%s",argv[1]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This i about argc and argv
